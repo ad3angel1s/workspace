245ff0269915f172064287c18da4f10cc8d412f5
Antonio de Angelis <antoniodeangelis1@gmail.com>
2016-04-03 23:42:36 +0100

    Further cleaning to cmake file and source code

--- c/src/static_alloc.c
+++ c/src/static_alloc.c
@@ -1,16 +1,24 @@
+#include <stdio.h>
+
 static int a = 4;
+
+int func1();
+int func2();
 
 int main()
 {
+    printf("%d + %d + %d = %d\n", a, func1(), func2(), a+func1()+func2() );
     return 0;
 }
 
-void func1()
+int func1()
 {
     static int a = 3;
+    return a;
 }
 
-void func2()
+int func2()
 {
     static int a = 2;
+    return a;
 }
