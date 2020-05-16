330e7a65811966fadc6a7d286e50ab68d775a7f3
Antonio de Angelis <antoniodeangelis1@gmail.com>
2020-05-16 23:03:13 +0100

    Make cross-compilation working correctly

--- hello-world/main.c
+++ hello-world/main.c
@@ -1,3 +1,14 @@
+/*
+ * Testable on QEMU:
+ * qemu-system-riscv64 -machine virt -m 128M -gdb tcp::1234 -S -kernel build/hello-world/hello-world
+ *
+ * debuggable by running a gdb instance:
+ * riscv64-unknown-elf-gdb --tui build/hello-world/hello-world
+ *
+ * and from there attach to the gdbserver:
+ * target remote :1234
+ */
+
 int main(void) {
     int a = 4;
     int b = 12;
